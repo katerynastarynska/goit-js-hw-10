4db68fcf77160c69df2d559c75d19b7c672a5c18
Kateryna Starynska <katerynastarynska@gmail.com>
2022-08-04 18:00:03 -0500

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+console.log(Notiflix)
+
+const debounce = require('lodash.debounce');
+console.log(debounce)
 
 const DEBOUNCE_DELAY = 300;
 console.log(DEBOUNCE_DELAY)
+
+const inputEl = document.querySelector('input[id=search-box]')
+console.log(inputEl)
+
+Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
+Notiflix.Notify.failure('Oops, there is no country with that name');
+
+fetch('https://restcountries.com/v3.1/name/united').then(response => {
+    return response.json();
+}).then(country => {
+    console.log(country);
+}).catch(error => {
+    console.log(error);
+})
